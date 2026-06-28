65740b83de61a224719fa4119cac4c2e8740f063
vivabaranwal <vivacodes03@gmail.com>
2026-06-28 17:42:55 +0530

    feat: build base copyright footer structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+﻿export default function Footer() {
+  return (
+    <footer style={{ padding: '2rem', textAlign: 'center', background: '#000' }}>
+      <p>Â© 2026 Viva Baranwal</p>
+    </footer>
+  )
+}
